fb9323fcc827a1a6b1aa20801c34157af833bb75
The-curs0r <6ak756yti@gmail.com>
2020-05-01 13:54:14 +0530

    Added simple intersections with spheres

--- RayTracer/Source.cpp
+++ RayTracer/Source.cpp
@@ -14,9 +14,34 @@ using namespace std;
 const int WIDTH = 600;
 const int HEIGHT = 600;
 const int MAXDEPTH = 5;
+double CAMERA[] = { -4, 0, 0, 1, 0, 0, 0, 1, 0, 30 };
 
 int main()
 {
+
+	Scene* scene = new Scene();
+
+	sphere* sphere1 = new sphere();
+	sphere1->center = glm::dvec3(10, 0, -2);
+	sphere1->radius = 2;
+	sphere1->diffuse = glm::dvec3(.6, .3, .9);
+	sphere1->ambient = glm::dvec3(.6, .3, .9);
+	sphere1->emission = glm::dvec3(.3, .5, .6);
+	sphere1->shininess = .2;
+	sphere1->specular = glm::dvec3(.3, .3, .3);
+
+	sphere* sphere2 = new sphere();
+	sphere2->center = glm::dvec3(5, 0, -2);
+	sphere2->ambient = glm::dvec3(.3, .5, .1);
+	sphere2->radius = 1;
+	sphere2->diffuse = glm::dvec3(.6, .3, .9);
+	sphere2->emission = glm::dvec3(.3, .5, .6);
+	sphere2->shininess = .2;
+	sphere2->specular = glm::dvec3(.3, .3, .3);
+
+	scene->add(sphere1);
+	scene->add(sphere2);
+
 	ofstream Output_Image("Output.ppm");
 	if (Output_Image.is_open())
 	{
@@ -25,8 +50,10 @@ int main()
 		{
 			for (int j = 0; j < WIDTH; j++)
 			{
-				glm::dvec3 pixColor = glm::dvec3(.200, .197, .184);
-				Output_Image << (int)(255 * pixColor[0]) << ' ' << (int)(255 * pixColor[0]) << ' ' << (int)(255 * pixColor[0]) << "\n";
+				ray* temp = new ray();
+				temp->raythrough(CAMERA, i + .5, j + .5, WIDTH, HEIGHT);
+				glm::dvec3 pixColor = scene->intersectray(*temp);
+				Output_Image << (int)(255 * pixColor[0]) << ' ' << (int)(255 * pixColor[1]) << ' ' << (int)(255 * pixColor[2]) << "\n";
 			}
 		}
 	}
--- RayTracer/scene.hpp
+++ RayTracer/scene.hpp
@@ -13,17 +13,33 @@ public:
 		objects.push_back(object);
 	}
 
-	const glm::dvec3 intersectray(ray& ray_int) {
+	const glm::dvec3 intersectray(ray& rayIn) {
 		
 		double minDistance = FLT_MAX;
+
+		glm::dvec3 outColor = glm::dvec3(0, 0, 0);
 
 		std::vector<object*>::iterator objIterator = objects.begin();
 
 		const object* int_object = nullptr;
 		glm::dvec3 int_point;
+		glm::dvec3 normal;
 		for (objIterator = objects.begin();objIterator != objects.end(); objIterator++) {
 			//Add interesection with objects here
+			double intersection = (*objIterator)->intersect(rayIn,normal);
+
+			if (intersection < minDistance && intersection > 0)
+			{
+				minDistance = intersection;
+				int_object = *objIterator;
+				int_point = rayIn.origin + intersection * rayIn.direction;
+			}
 		}
+		if (int_object != nullptr)
+		{
+			outColor = int_object->ambient;
+		}
+		return outColor;
 	}
 
 };
